1b3af078500e45cf9e4c6c0b71e75ce0db4e6bae
GApxo <gioapkhadze@gmail.com>
2024-10-31 00:34:57 +0400

    123

--- javscript/clock/script.js
+++ javscript/clock/script.js
@@ -3,14 +3,38 @@ const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
 const timeEl = document.querySelector(".time");
 const dateEl = document.querySelector(".date");
-
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednsday",
+  "Thurysday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "july",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
 function setTime() {
   const time = new Date();
   const month = time.getMonth();
   const day = time.getDay();
-  const hours = time.getHours() % 12; 
+  const date = time.getDate();
+  const hours = time.getHours() % 12;
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
+  const amp = hours >= 12 ? "PM" : "AM";
 
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     hours,
@@ -33,11 +57,10 @@ function setTime() {
     0,
     360
   )}deg)`;
+
+  timeEl.innerHTML = `${hours}: ${minutes} ${amp}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} ${date}`;
 }
-
-timeEl.innerHTML = `${}`
-
-
 
 setTime();
 
--- javscript/objects/script.js
+++ javscript/objects/script.js
@@ -42,32 +42,55 @@ const productDate = new Date(data?.publish_date);
 // const milliseconds = myDate.getMilliseconds();
 // console.log("milliseconds", milliseconds);
 
-let span = document.getElementById("output");
+// let span = document.getElementById("output");
 
-function time() {
-  let welcome;
-  const newDate = new Date();
+// function time() {
+//   let welcome;
+//   const newDate = new Date();
 
-  let hours = newDate.getHours();
-  let minutes = newDate.getMinutes();
-  let seconds = newDate.getSeconds();
+//   let hours = newDate.getHours();
+//   let minutes = newDate.getMinutes();
+//   let seconds = newDate.getSeconds();
 
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
+//   if (minutes < 10) {
+//     minutes = "0" + minutes;
+//   }
 
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
+//   if (seconds < 10) {
+//     seconds = "0" + seconds;
+//   }
 
-  if (hours < 12) {
-    welcome = "Good Morning";
-  } else if (hours < 18) {
-    welcome = "Good Afternoon";
-  } else {
-    welcome = "Good Evening";
-  }
+//   if (hours < 12) {
+//     welcome = "Good Morning";
+//   } else if (hours < 18) {
+//     welcome = "Good Afternoon";
+//   } else {
+//     welcome = "Good Evening";
+//   }
 
-  span.textContent = welcome + " " + hours + ":" + minutes + ":" + seconds;
-}
-setInterval(time, 1000);
+//   span.textContent = welcome + " " + hours + ":" + minutes + ":" + seconds;
+// }
+// setInterval(time, 1000);
+
+let x = -25;
+console.log(Math.abs(x));
+
+const max = Math.max(15, 45); // 45
+const min = Math.min(14, 45); // 14
+
+const newMax = Math.max(1, 2, 3, 4, 5 - 9, -32, 68); // 68
+
+const y = Math.round(9.5); // 10
+
+const random = Math.random();
+console.log(random);
+
+const pow = Math.pow(2, 3);
+console.log(pow);
+
+const sqr = Math.sqrt(121); // 11
+
+const test = Math.atan(1);
+console.log(test);
+
+console.log(Math.PI);
